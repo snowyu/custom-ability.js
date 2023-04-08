27d87f3251488320a2af65fc9129acb5c734142e
Riceball LEE <snowyu.lee@gmail.com>
2023-04-08 16:32:12 +0800

    feat: export requireAbility named function

--- src/require.ts
+++ src/require.ts
@@ -6,14 +6,15 @@ async function getDefaultEntry(name: string) {
   return result;
 }
 
-export default async function(packageName, aClass, aOptions) {
+export async function requireAbility(packageName, aClass, aOptions) {
   let addAbility: Function;
   try {
     addAbility = await getDefaultEntry(packageName + '/ability.js');
   } catch (error) {}
   if (typeof addAbility !== "function") {
-    throw TypeError('the package no ability supports.');
+    throw new TypeError('the package no ability supports.');
   }
   return addAbility(aClass, aOptions);
 };
 
+export default requireAbility
