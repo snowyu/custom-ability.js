e76634fbfe720e8c0afdd2f93916baaaa7ebeeac
Riceball LEE <snowyu.lee@gmail.com>
2023-04-08 16:33:48 +0800

    feat: export all functions on the index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import { createAbilityInjector } from "./custom-ability";
+import { createAbilityInjector } from './custom-ability'
+
+export * from './custom-ability'
+export * from './has-ability-on-parent'
+export * from './injected-on-parent'
+export * from './require'
 
 export default createAbilityInjector
